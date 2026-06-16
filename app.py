8d4c842062ab06aad21e473b74a8367abfa48612
renzcoding <renzcoding@gmail.com>
2026-06-16 23:28:12 +0700

    change secreet

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def dapatkan_respon_gemini(messages_history):
 
     model = genai.GenerativeModel(
 
-        model_name="gemini-1.5-flash",
+        model_name="gemini-2.5-flash",
 
         system_instruction=SYSTEM_INSTRUCTION
 
